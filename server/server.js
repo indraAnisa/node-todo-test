d3b05c9454430aeb3521e4f936811106eafc6c6e
codewaster <codewaster@bitbucket.org>
2018-02-26 09:43:51 +0700

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -2,31 +2,50 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 
-var {mongoose} = require('./db/mongoose');
-var {Users} = require('./models/user');
-var {Todo} = require('./models/todo');
- 
+var {
+    mongoose
+} = require('./db/mongoose');
+var {
+    Users
+} = require('./models/user');
+var {
+    Todo
+} = require('./models/todo');
+
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res)=>{
+app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
 
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    },(e)=>{
+    }, (e) => {
+        res.status(404).send(e);
+    })
+})
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+
+    }, e => {
         res.status(404).send(e);
     })
 })
 
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('started on port 3000');
 })
- 
 
-module.exports = {app};
+
+module.exports = {
+    app
+};
