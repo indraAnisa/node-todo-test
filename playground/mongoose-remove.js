0c989a0313a7785ff64de41c372e4d6f3ef4d6c0
codewaster <codewaster@bitbucket.org>
2018-02-26 21:39:32 +0700

    test remove

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,6 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const{Users} = require('./../server/models/user');
+
+const{ObjectID} = require('mongodb');
+
