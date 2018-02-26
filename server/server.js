d3ac2ce7baba0bd4d6141660e9f30595c3f703fe
codewaster <codewaster@bitbucket.org>
2018-02-26 11:30:48 +0700

    add GET / /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,51 +1,67 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
+var { ObjectID } = require("mongodb");
 
-
-var {
-    mongoose
-} = require('./db/mongoose');
-var {
-    Users
-} = require('./models/user');
-var {
-    Todo
-} = require('./models/todo');
-
+var { mongoose } = require("./db/mongoose");
+var { Users } = require("./models/user");
+var { Todo } = require("./models/todo");
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-    var todo = new Todo({
-        text: req.body.text
+app.post("/todos", (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(404).send(e);
+    }
+  );
+});
+
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({
+        todos
+      });
+    },
+    e => {
+      res.status(404).send(e);
+    }
+  );
+});
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  //validate id using isValid
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (todo) {
+        res.status(200).send({todo});
+      } else {
+        res.status(404).send();
+      }
+    })
+    .catch(e => {
+      res.status(400).send();
     });
-
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.status(404).send(e);
-    })
-})
-
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({
-            todos
-        });
-
-    }, e => {
-        res.status(404).send(e);
-    })
-})
-
+});
 
 app.listen(3000, () => {
-    console.log('started on port 3000');
-})
-
+  console.log("started on port 3000");
+});
 
 module.exports = {
-    app
+  app
 };
