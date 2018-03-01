3722dcb13dc1aafb84aca9a9fb8320eeab97d7f9
codewaster <codewaster@bitbucket.org>
2018-03-01 11:51:11 +0700

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var { ObjectID } = require("mongodb");
+var _ = require("lodash");
 
 var { mongoose } = require("./db/mongoose");
 var { Users } = require("./models/user");
@@ -80,6 +81,41 @@ app.delete("/todos/:id", (req, res) => {
       res.status(404).send();
     }
   });
+});
+
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+
+  //validate id using isValid
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(
+    id,
+    {
+      $set: body
+    },
+    { new: true }
+  )
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    })
+    .catch(e => {
+      res.status(404).send();
+    });
 });
 
 app.listen(port, () => {
