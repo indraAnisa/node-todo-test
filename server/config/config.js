be7065df404e68f4cc512d4d20b7e5756d59405f
codewaster <codewaster@bitbucket.org>
2018-03-08 20:55:21 +0700

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,10 @@
 var env = process.env.NODE_ENV || "development";
 
-if (env === "development") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/todosapp_db";
-} else if (env === "test") {
-  process.env.port = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/todosapp_db";
-} else {
-  //production
-  process.env.MONGODB_URI =
-    "mongodb://joko:jokopret@ds021289.mlab.com:21289/todosapp_db";
+if (env === "development" || env === "test") {
+  var config = require("./config.json");
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach(key => {
+    process.env[key] = envConfig[key];
+  });
 }
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,52 +1,54 @@
-const {
-  ObjectID
-} = require("mongodb");
+const { ObjectID } = require("mongodb");
 const jwt = require("jsonwebtoken");
 
-const {
-  Todo
-} = require("./../../models/todo");
-const {
-  User
-} = require("./../../models/user");
+const { Todo } = require("./../../models/todo");
+const { User } = require("./../../models/user");
 
 const userOneId = new ObjectID();
 const userTwoId = new ObjectID();
-const users = [{
+const users = [
+  {
     _id: userOneId,
     email: "indra@example.com",
     password: "userOnePass",
-    tokens: [{
-      access: "auth",
-      token: jwt
-        .sign({
-            _id: userOneId,
-            access: "auth"
-          },
-          "abc123"
-        )
-        .toString()
-    }]
+    tokens: [
+      {
+        access: "auth",
+        token: jwt
+          .sign(
+            {
+              _id: userOneId,
+              access: "auth"
+            },
+            process.env.JWT_SECRET
+          )
+          .toString()
+      }
+    ]
   },
   {
     _id: userTwoId,
     email: "jen@example.com",
     password: "userTwoPass",
-    tokens: [{
-      access: "auth",
-      token: jwt
-        .sign({
-            _id: userTwoId,
-            access: "auth"
-          },
-          "abc123"
-        )
-        .toString()
-    }]
+    tokens: [
+      {
+        access: "auth",
+        token: jwt
+          .sign(
+            {
+              _id: userTwoId,
+              access: "auth"
+            },
+            process.env.JWT_SECRET
+          )
+          .toString()
+      }
+    ]
   }
 ];
 
-const todos = [{
+const todos = [
+  {
     _id: new ObjectID(),
     text: "First test todo",
     _creator: userOneId
